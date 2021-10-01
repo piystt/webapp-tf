492fbbe4e5995d42fe5196614fc396f826a5bed0
Terence Yip <ttsyip_work@outlook.com>
2021-10-01 16:35:21 +0200

    Update Commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,3 +47,42 @@ resource "azurerm_app_service" "website" {
     scm_type         = "LocalGit"
   }
 }
+
+resource "azurerm_log_analytics_workspace" "log" {
+  name                = "tyip1234-lg-analytics"
+  location            = data.azurerm_resource_group.wsdevops.location
+  resource_group_name = data.azurerm_resource_group.wsdevops.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "appi" {
+  name                = "tyip1234-api"
+  location            = data.azurerm_resource_group.wsdevops.location
+  resource_group_name = data.azurerm_resource_group.wsdevops.name
+  workspace_id        = azurerm_log_analytics_workspace.log.id
+  application_type    = "web"
+}
+
+// Skeleton for linking web app and app insights
+resource "null_resource" "link_monitoring" {
+  provisioner "local-exec" {
+    command = <<EOT
+      # Login to Azure CLI (Linux operating system assumed)
+      az login --service-principal -u $con_client_id -p $con_client_secret --tenant $con_tenant_id
+      az webapp config appsettings set --name $web_app_name --resource-group $rg_name --settings APPINSIGHTS_INSTRUMENTATIONKEY=$inst_key APPINSIGHTS_PROFILERFEATURE_VERSION=1.0.0 APPINSIGHTS_SNAPSHOTFEATURE_VERSION=1.0.0 APPLICATIONINSIGHTS_CONNECTION_STRING=$conn_str ApplicationInsightsAgent_EXTENSION_VERSION=~3 DiagnosticServices_EXTENSION_VERSION=~3 InstrumentationEngine_EXTENSION_VERSION=disabled SnapshotDebugger_EXTENSION_VERSION=disabled XDT_MicrosoftApplicationInsights_BaseExtensions=recommended XDT_MicrosoftApplicationInsights_PreemptSdk=disabled
+      # TODO your scripting code
+    EOT
+    environment = {
+      // Parameters needed to login
+      con_client_id     = TODO
+      con_client_secret = TODO
+      con_tenant_id     = TODO
+      // Parameters needed for linking
+      inst_key          = TODO
+      conn_str          = TODO      
+      rg_name           = TODO
+      web_app_name      = TODO
+    }
+  }
+}
